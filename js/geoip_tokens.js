2862b197d11270b5594b0df82b848a5c3b512ad9
indigoxela <user@olga.indigofloat.at>
2021-05-04 12:17:08 +0200

    Add js helper function for ajax

--- js/geoip_tokens.js
+++ js/geoip_tokens.js
@@ -1,3 +1,31 @@
 (function ($) {
-  console.log('No clue where to start.');
+
+  'use strict';
+
+  Backdrop.geoipTokens = {};
+
+/**
+ * To make use of Backdrop.geoipTokens.getData():
+ *
+ * Backdrop.geoipTokens.getData('latlon').success(function (data) {
+ *   // Your callback here.
+ * });
+ *
+ * Available types:
+ * - latlon: latitude and longitude
+ * - ccode: country code (uppercase)
+ * - tzone: timezone name
+ */
+
+  Backdrop.geoipTokens.getData = function (type) {
+    return $.ajax({
+      url: Backdrop.settings.basePath + 'geoip-tokens/ajax/' + type,
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        geoipTokenRequestToken: Backdrop.settings.geoipToken
+      }
+    });
+  }
+
 })(jQuery);
